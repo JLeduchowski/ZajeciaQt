4bd29b8dd8216d15334ab0802b100c084571e6fa
milewskim <maciej.milewski@unilodz.eu>
2020-11-19 10:56:36 +0100

    usunięcie zbędnego pliku

--- Zadanie01_MaciejMilewski.py
+++
@@ -1,9 +0,0 @@
-
-print(f'Lustrzane odbicie podanego ciagu znakow.')
-string = input("Podaj ciag znakow, quit zakonczy skrypt.")
-
-while(string!='quit'):
-    print(f'Podales {string}')
-    string = string[::-1]
-    print(f'Lustrzane odbicie: {string}')
-    string = input("Podaj ciag znakow, quit zakonczy skrypt.")
